6a52e6fe61df9d43b5fc07f892657fc6ce8a0114
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2021-09-18 18:02:10 -0400

    Fixed bug to close program and added unique UserAgent to every user

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const args = [
     '--window-position=0,0',
     '--ignore-certifcate-errors',
     '--ignore-certifcate-errors-spki-list',
-    '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.0 Safari/537.36"'
+    // '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.0 Safari/537.36"'
     ];
 
 
-const scrapeProduct = (async () => {
-    // Get passed-in data
-    const ingredient = '3';
-
+const scrapeProduct = (async (ingredient) => {
     // Start puppeteer
     const browser = await puppeteer.launch({
         headless: false,
         args
     });
 
-    // Create a new page to walmart
+    // Set user-agent based on system
     const page = await browser.newPage();
+    await page.setUserAgent(await browser.userAgent());
+
+    // Create a new page to walmart
     const popularQuery = '&sort=Popular%3ADESC';
     await Promise.all([
         // Go to the ingredient with the popular query
@@ -58,31 +58,31 @@ const scrapeProduct = (async () => {
             ppg: priceParsed[1] ? priceParsed[1] : 'NA',
         });
     }
-    console.log(productsInfo);
     
     // Check if the page got blocked
     if (page.url().includes('blocked')){
         // Restart the browser
         await browser.close();
-        await scrapeProduct();
+        await scrapeProduct(ingredient);
     }
 
     // Check if the ingredient was valid
     if (productsInfo.length === 0) {
         await browser.close();
-        return {message: `The ingredient: '${ingredient}' is not valid.`}
+        return { message: `The ingredient: '${ingredient}' is not valid.` }
     }
 
-
-    // TODO: Check if the ingredients is null!
-
-
     // For now, check if the first three are valid 'ingredients'
-
-
+    for (const product of productsInfo.slice(0, 3)){
+        if (!(product.name.toLowerCase().includes(ingredient))){
+            return { message: `The ingredient (${ ingredient }) did not match on Walmart.` }
+        }
+    }
+    return productsInfo.slice(0,3);
 });
 
 (async () => {
-    const p = await scrapeProduct();
+    const p = await scrapeProduct('chicken');
     console.log(p);
+    process.exit(0);
 })();
